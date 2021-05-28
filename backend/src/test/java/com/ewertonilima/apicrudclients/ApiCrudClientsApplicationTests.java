be0e31ab2d9ea09e6c93bab891ee6b9b6785c5af
EwertonILima <ewertoninaciolima@gmail.com>
2021-05-28 13:29:10 -0300

    Project Created

---
+++ backend/src/test/java/com/ewertonilima/apicrudclients/ApiCrudClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewertonilima.apicrudclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
