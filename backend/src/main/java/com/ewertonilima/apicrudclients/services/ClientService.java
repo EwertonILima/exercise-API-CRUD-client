3782d699e68eb60a94bc770173ec265489a8b2dc
EwertonILima <ewertoninaciolima@gmail.com>
2021-05-31 17:10:10 -0300

    Get by id

--- backend/src/main/java/com/ewertonilima/apicrudclients/services/ClientService.java
+++ backend/src/main/java/com/ewertonilima/apicrudclients/services/ClientService.java
@@ -1,9 +1,12 @@
 package com.ewertonilima.apicrudclients.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ewertonilima.apicrudclients.dto.ClientDTO;
 import com.ewertonilima.apicrudclients.entities.Client;
@@ -15,8 +18,15 @@ public class ClientService {
 	@Autowired
 	ClientRepository clientRepository;
 
+	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Client> list = clientRepository.findAll(pageRequest);
 		return list.map(x -> new ClientDTO(x));
+	}
+
+	public ClientDTO findByid(Long id) {
+		Optional<Client> obj = clientRepository.findById(id);
+		Client entity = obj.get();
+		return new ClientDTO(entity);
 	}
 }
