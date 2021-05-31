88252fd24377dc9ebd4f63ed6b6ca4bf98ec07bb
EwertonILima <ewertoninaciolima@gmail.com>
2021-05-31 14:56:31 -0300

    Entity, H2, Seed

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Maria da Silva', '80758017090', 3000.0, TIMESTAMP WITH TIME ZONE '1965-07-13T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('João Pereira', '27767073041', 2000.0, TIMESTAMP WITH TIME ZONE '1970-01-01T20:50:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Pedro Freitas', '03654951074', 4000.0, TIMESTAMP WITH TIME ZONE '1960-03-29T20:50:07.12345Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Lucas Silva e Silva', '09622913075', 5000.0, TIMESTAMP WITH TIME ZONE '1966-04-20T20:50:07.12345Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Pompeu Pompilho Pomposo', '07825368088', 2500.0, TIMESTAMP WITH TIME ZONE '1971-09-20T20:50:07.12345Z', 5);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Maria Gertrudes', '47222348093', 3400.0, TIMESTAMP WITH TIME ZONE '1965-08-11T20:50:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Antonio Fagundes', '74690727058', 4200.0, TIMESTAMP WITH TIME ZONE '1960-12-15T20:50:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Carlos Vilagran', '42877273040', 4500.0, TIMESTAMP WITH TIME ZONE '1959-06-28T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('Roberto Bollanos', '17305726001', 4800.0, TIMESTAMP WITH TIME ZONE '1975-04-14T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) values ('José Silvino', '65113281042', 2900.0, TIMESTAMP WITH TIME ZONE '1980-08-13T20:50:07.12345Z', 0);
