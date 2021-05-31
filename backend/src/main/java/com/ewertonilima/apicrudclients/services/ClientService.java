c9a6c10892772aa5fca11d0180dc95b398e91680
EwertonILima <ewertoninaciolima@gmail.com>
2021-05-31 20:04:50 -0300

    Delete, Database exception

--- backend/src/main/java/com/ewertonilima/apicrudclients/services/ClientService.java
+++ backend/src/main/java/com/ewertonilima/apicrudclients/services/ClientService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.ewertonilima.apicrudclients.dto.ClientDTO;
 import com.ewertonilima.apicrudclients.entities.Client;
 import com.ewertonilima.apicrudclients.repositories.ClientRepository;
+import com.ewertonilima.apicrudclients.services.exceptions.DatabaseException;
 import com.ewertonilima.apicrudclients.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -52,6 +55,18 @@ public class ClientService {
 		}
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id " + id + " not found ");
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+			clientRepository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id " + id + " not found ");
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 
