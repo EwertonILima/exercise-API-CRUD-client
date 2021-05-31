f5cc59adcbc6c4eb2f52d4723a65c1a0decbd8bc
EwertonILima <ewertoninaciolima@gmail.com>
2021-05-31 17:01:16 -0300

    DTO

--- backend/src/main/java/com/ewertonilima/apicrudclients/controllers/ClientController.java
+++ backend/src/main/java/com/ewertonilima/apicrudclients/controllers/ClientController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ewertonilima.apicrudclients.entities.Client;
+import com.ewertonilima.apicrudclients.dto.ClientDTO;
 import com.ewertonilima.apicrudclients.services.ClientService;
 
 @RestController
@@ -21,14 +21,15 @@ public class ClientController {
 	ClientService clientService;
 
 	@GetMapping
-	public ResponseEntity<Page<Client>> findAll(
+	public ResponseEntity<Page<ClientDTO>> findAll(
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
 			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
 			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
 			) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		Page<Client> list = clientService.findAllPaged(pageRequest);
+		
+		Page<ClientDTO> list = clientService.findAllPaged(pageRequest);
 		return ResponseEntity.ok().body(list);
 	}
 
--- backend/src/main/java/com/ewertonilima/apicrudclients/services/ClientService.java
+++ backend/src/main/java/com/ewertonilima/apicrudclients/services/ClientService.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import com.ewertonilima.apicrudclients.dto.ClientDTO;
 import com.ewertonilima.apicrudclients.entities.Client;
 import com.ewertonilima.apicrudclients.repositories.ClientRepository;
 
@@ -14,8 +15,8 @@ public class ClientService {
 	@Autowired
 	ClientRepository clientRepository;
 
-	public Page<Client> findAllPaged(PageRequest pageRequest) {
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Client> list = clientRepository.findAll(pageRequest);
-		return list;
+		return list.map(x -> new ClientDTO(x));
 	}
 }
